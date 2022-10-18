0f11a3cd1e85626e504881ae8ff8eb7fdfb9d043
deidrye <91196803+deidrye@users.noreply.github.com>
2022-10-18 11:41:43 +0300

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            FailProcess();
+        }
+        catch { }
+
+        Console.WriteLine("Failed to fail process!");
+        Console.ReadKey();
+    }
+
+    static void FailProcess()
+    {       //... write your code here 	}
+
+        // Environment.Exit(0);
+
+        // Process.GetCurrentProcess().Kill();
+
+        /*
+        string procName = Process.GetCurrentProcess().ProcessName;
+        foreach (Process process in Process.GetProcessesByName(procName))
+        {
+            process.Kill();
+        }
+        */
+
+        /*
+        int procId = Process.GetCurrentProcess().Id;
+        Process.GetProcessById(procId).Kill();
+        */
+
+    }
+
+
+}
