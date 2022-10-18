83856676d713eccd6e981b8a2546537006cc9ecc
deidrye <91196803+deidrye@users.noreply.github.com>
2022-10-18 11:43:07 +0300

    Add files via upload

---
+++ Task5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TransformToElephant();
+        Console.Out.WriteLine("Муха");
+
+        //... custom application code
+        int i = -7 + 8;
+        Console.Write(i + "-ая ");
+        Console.Out.WriteLine("Муха");
+    }
+
+    static void TransformToElephant()
+    {
+        Console.WriteLine("Слон");
+        Console.SetOut(new AbstractOut());          // следующая консольная запись будет перенаправлена в абстрактный поток
+    }
+
+    class AbstractOut : StringWriter
+    {
+        TextWriter AConsole;
+
+        public AbstractOut()
+        {
+            AConsole = Console.Out;
+        }
+
+        public override void WriteLine(string value)        // отменяем вывод в консоль
+        {
+            Console.SetOut(AConsole);
+        }
+    }
+
+}
+
